db49a05ecf824c5f5718482c3f33811e669ea5a8
joemcpartland@gmail.com <josephmcpartland@JM.hsd1.co.comcast.net>
2021-03-10 17:34:20 -0700

    commit tree 30b98b8e5b80475264ddf38da4b52f0ff0b81d3e

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,11 @@
+require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+puts doc.css(".site-header__hero__headline").text
+
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+puts doc.css(".headline-26OIBN").text
+
+# binding.pry
